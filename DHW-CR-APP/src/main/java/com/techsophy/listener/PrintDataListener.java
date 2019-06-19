4c8504b283022e92796416b5184818f3a93b6f29
sravan <sravan.d@techsophy.com>
2019-06-19 15:52:02 +0530

    added custom listener

--- DHW-CR-APP/src/main/java/com/techsophy/listener/PrintDataListener.java
+++ DHW-CR-APP/src/main/java/com/techsophy/listener/PrintDataListener.java
@@ -8,8 +8,10 @@ import org.apache.log4j.Logger;
 
 public class PrintDataListener implements TaskListener {
 	static final Logger logger = Logger.getLogger(PrintDataListener.class);
+	
 	@Override
 	public void notify(DelegateTask delegateTask) {
+		
 		System.out.println("-----------------------Custom Listener in PrintDataListener----------------------------");
 		logger.info("------------------------Custom Listener in PrintDataListener-------------------------------");
 		
